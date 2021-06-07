767aa3aeecda6819fc15a0cd2712b824595b8e18
unknown <motley.dylan60@gmail.com>
2021-06-06 22:14:57 -0400

    Clean up / Name changes

---
+++ MySQL/food-db/food-db-recalculate-total-1.sql
@@ -0,0 +1,11 @@
+set @order_total = (select sum(price * Quantity) as total
+  from lineitem
+  join menuitem mi    on MenuItemID = mi.id
+ where orderticketid = 2
+ group by OrderTicketID);
+
+update orderticket
+    set total = @order_total
+where id = 2;
+
+select * from orderticket;
